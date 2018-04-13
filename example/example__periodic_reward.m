9cd6c17b1de2a15f3a62baf9f0d1b3879e6e0033
nick <nick@fagan.org>
2018-04-13 17:11:04 -0400

    latest config files

--- example/example__periodic_reward.m
+++ example/example__periodic_reward.m
@@ -1,11 +1,11 @@
 reward_period = 2000; % ms
-reward_amount = 50;  % ms
-total_time = 40;  % s
+reward_amount = 100;  % ms
+total_time = 5*60;  % s
 
 %   1 -> m1;
 %   2 -> m2;
 
-rewarded_targets = 2;
+rewarded_targets = 1;
 % rewarded_targets = 1:2;
 
 brains.task.periodic_reward( total_time, reward_period, reward_amount, rewarded_targets );
