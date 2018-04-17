6867834f74d6782e8968c3a032ab70424909b07d
nick <nick@fagan.org>
2018-04-17 12:47:31 -0400

    begin synch

--- +brains/+arduino/+calino/+bound_funcs/face_top_and_bottom.m
+++ +brains/+arduino/+calino/+bound_funcs/face_top_and_bottom.m
@@ -1,5 +1,41 @@
 function bounds = face_top_and_bottom(calibration_data, key_map, padding_info, const)
 
+facebl = key_map( 'facebl' );
+facetl = key_map( 'facetl' );
+facebr = key_map( 'facebr' );
+facetr = key_map( 'facetr' );
 
+facebl = brains.arduino.calino.get_coord( calibration_data, facebl );
+facetl = brains.arduino.calino.get_coord( calibration_data, facetl );
+facebr = brains.arduino.calino.get_coord( calibration_data, facebr );
+facetr = brains.arduino.calino.get_coord( calibration_data, facetr );
+
+x1 = facebl(1);
+x2 = facebr(1);
+x3 = facetl(1);
+x4 = facetr(1);
+
+y1 = facebl(2);
+y2 = facebr(2);
+y3 = facetl(2);
+y4 = facetr(2);
+
+width_px = mean( [x2-x1, x3-x4] );
+width_cm = const.FACE_WIDTH_CM;
+ratio = width_px / width_cm;
+
+x0 = mean( [x1, x3] );
+x1 = mean( [x2, x4] );
+
+y0 = mean( [y1, y3] );
+y1 = mean( [y2, y4] );
+
+x0 = x0 - (padding_info.face.x * ratio);
+x1 = x1 + (padding_info.face.x * ratio);
+
+y0 = y0 - (padding_info.face.y * ratio);
+y1 = y1 + (padding_info.face.y * ratio);
+
+bounds = [ x0, y0, x1, y1 ];
 
 end
--- example/example__brains_eyelink.m
+++ example/example__brains_eyelink.m
@@ -41,7 +41,7 @@ import brains.arduino.calino.bound_funcs.both_eyes;
 calibration_data = brains.util.get_latest_far_plane_calibration( [], false );
 padding_info = brains.arduino.calino.define_padding();
 consts = brains.arduino.calino.define_calibration_target_constants();
-key_map = brains.arduino.calino.get_outer_calibration_key_roi_map();
+key_map = calibration_data.key_name_map;
 
 padding_info.eyes.x = 2.75;
 padding_info.eyes.y = 2.75;
@@ -57,7 +57,7 @@ send_bounds( comm, 'm2', 'eyes', [1366, 316, 1460, 363] );
 ids = brains.arduino.calino.get_ids();
 
 send_stim_param( comm, 'all', 'probability', 100 );
-send_stim_param( comm, 'all', 'frequency', 1000 );
+send_stim_param( comm, 'all', 'frequency', 100rr );
 send_stim_param( comm, 'all', 'stim_stop_start', 0 );
 send_stim_param( comm, 'eyes', 'stim_stop_start', 1 );
 send_stim_param( comm, 'all', 'protocol', ids.stim_protocols.probabilistic );
